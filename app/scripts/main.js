f7560fefd4afe2731975234a3fdfeb00736f7552
Marie Malherbe <mariemalherbe@iMac-de-Fabien.local>
2017-03-03 13:11:46 +0100

    add scene3 scrollMagice

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,21 +11,29 @@ var controller = new ScrollMagic.Controller()
         bloc = $('.section__2').find('.content'),
         after = $('.section__2').find('h2:after');
 
-    var tl = new TimelineMax({pause: false});
-    tl.add("start") // add timeline label
-      .fromTo(text, 0.5, { x: '100px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
-      .fromTo(btn, 0.5, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
-      .fromTo(bloc, 0.5, { x: '100px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
-      .fromTo(img, 0.5, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "+=0.05" )
-      .fromTo(after, 0.5, { x: '100px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "+=0.05" )
+        var tl = new TimelineMax({pause: false});
+        tl.add("start") // add timeline label
+          .fromTo(text, 0.5, { x: '100px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+          .fromTo(btn, 0.5, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+          .fromTo(bloc, 0.5, { x: '100px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+          .fromTo(img, 0.5, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "+=0.05" )
+          .fromTo(after, 0.5, { x: '100px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "+=0.05" )
 
-    var para = $('.section__3').find('.paragraph');
-        image = $('.section__3').find('.img__3');
+        var para = $('.section__3').find('.paragraph');
+            image = $('.section__3').find('.img__3');
 
-    var tlPortfolio = new TimelineMax({pause: false});
-    tlPortfolio.add('portfolio')
-    .fromTo(para, 1, { x: '200px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, 'portfolio')
-    .fromTo(image, 1, { y: '200px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, 'portfolio')
+        var tlPortfolio = new TimelineMax({pause: false});
+        tlPortfolio.add('portfolio')
+        .fromTo(para, 1, { x: '200px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, 'portfolio')
+        .fromTo(image, 1, { y: '200px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, 'portfolio')
+
+        var slider = $('.section__4').find('.content'),
+            name = $('.swiper-slide').find('.title-name');
+
+        var tlSlider = new TimelineMax({pause : false});
+        tlSlider.add('slider')
+        .fromTo(slider, 1, { x: '200px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, 'slider')
+        .fromTo(name, 1, { y: '300px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, 'slider')
 
 
 //Build a scene
@@ -40,9 +48,14 @@ var scene2 = new ScrollMagic.Scene({
 }).addIndicators()
 .setTween(tlPortfolio);
 
+var scene3 = new ScrollMagic.Scene({
+  triggerElement: '.section__4 .content',
+}).addIndicators()
+.setTween(tlSlider);
 controller.addScene([
 scene,
-scene2
+scene2,
+scene3
 ]);
 
 });
